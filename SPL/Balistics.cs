29a12364b34b9fed557e1827df560b969c882686
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-24 15:48:11 +0100

    new thing just done

---
+++ SPL/Balistics.cs
@@ -0,0 +1,75 @@
+﻿using MatrixTimesMatrix;
+using System.Net;
+using System.Security.AccessControl;
+
+namespace SPL
+{
+    static public class Balistics
+    {
+        static public double[,] GetEndpoint(double[,] startPoint, double gravityForce, double mass, double energy, double angle, double time)
+        {
+            double[,] endPoint = new double[,] { { } };
+
+            if (Validation(startPoint, endPoint, gravityForce, mass, energy, angle, time))
+            {
+                double speed = GetSpeed(energy, mass);
+
+
+                return endPoint;
+            }
+            else
+            {
+                throw new Exception("there's something wrong in your values");
+            }
+
+        }
+        static bool Validation(double[,] startPoint, double[,] endPoint, double gravityForce, double mass, double energy, double angle, double time)
+        {
+            if (MTM.CheckForPossibleAdditionOrSubstraction(endPoint, startPoint))
+            {
+                if (mass > 0)
+                {
+                    if (energy >= 0)
+                    {
+                        if (angle > 0)
+                        {
+                            if (time > 0)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                throw new Exception("the time value is not valid (must be above 0)");
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("the angle value is not valid (must be above 0 and bleow 360)");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("the energy value is not valid (must be above 0)");
+                    }
+                }
+                else
+                {
+                    throw new Exception("the mass value is not valid (must be above 0)");
+                }
+            }
+            else
+            {
+                throw new Exception("the startingPoint value is not valid ( use double[,]{ {X, Y} } )");
+            }
+
+        }
+        static double GetSpeed(double energy, double mass)
+        {
+            // Ek = (m * v^2) : 2
+            // 2 * Ek = m * v^2
+            // (Ek * 2) : m = v^2
+            // v = sqrt( (Ek * 2) : 2 )
+            return (Math.Sqrt( (energy * 2 ) / mass ));
+        }
+    }
+}
---
+++ SPL/MTM.cs
@@ -0,0 +1,176 @@
+﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MatrixTimesMatrix
+{
+    public static class MTM
+    {
+        /// <summary>
+        /// this method gets number of columns in matrix
+        /// </summary>
+        /// <param name="matrix">two dimentional array (double[,])</param>
+        /// <returns>number of columns in matrix (int)</returns>
+        public static int GetNumberOfColumns(double[,] matrix)
+        {
+            int numberOfColumns = matrix.GetLength(1); ;
+            return numberOfColumns;
+        }
+        /// <summary>
+        /// this method gets number of rows in matrix
+        /// </summary>
+        /// <param name="matrix">two dimentional array (double[,])</param>
+        /// <returns>number of rows in matrix (int)</returns>
+        public static int GetNumberOfRows(double[,] matrix)
+        {
+            int numberOfRows = matrix.GetLength(0); ;
+            return numberOfRows;
+        }
+        /// <summary>
+        /// this method checks if multipication is possible
+        /// </summary>
+        /// <param name="matrix1">first two dimentional array (double[,])</param>
+        /// <param name="matrix2">second two dimentional array (double[,])</param>
+        /// <returns>isPossible (bool)</returns>
+        public static bool CheckForPossibleMultiplication(double[,] matrix1, double[,] matrix2)
+        {
+            bool isPossible = false;
+            if (GetNumberOfColumns(matrix1) == GetNumberOfRows(matrix2))
+            {
+                isPossible = true;
+            }
+            return isPossible;
+        }
+        /// <summary>
+        /// this method checks if addition and substraction are possible
+        /// </summary>
+        /// <param name="matrix1">
+        /// first two dimentional array (double[,])
+        /// </param>
+        /// <param name="matrix2">second two dimentional array (double[,])</param>
+        /// <returns>isPossible (bool)</returns>
+        public static bool CheckForPossibleAdditionOrSubstraction(double[,] matrix1, double[,] matrix2)
+        {
+            bool isPossible = false;
+            if ((GetNumberOfColumns(matrix1) == GetNumberOfColumns(matrix2)) && (GetNumberOfRows(matrix1) == GetNumberOfRows(matrix2)))
+            {
+                isPossible = true;
+            }
+            return isPossible;
+        }
+        /// <summary>
+        /// this method multiplies 2 matrices 
+        /// </summary>
+        /// <param name="matrix1">first two dimentional array (double[,])</param>
+        /// <param name="matrix2">second two dimentional array (double[,])</param>
+        /// <returns>result matrix (double[,])</returns>
+        /// <exception cref="Exception">
+        /// if <c>CheckForPossibleMultiplication</c> for these 2 matrices is false,
+        /// it throws "invalid matrix size" - it means you should check your matrices numbers of columns and rows 
+        /// </exception>
+        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+        {
+            double[,] result;
+            if (CheckForPossibleMultiplication(matrix1, matrix2))
+            {
+                result = new double[GetNumberOfRows(matrix1), GetNumberOfColumns(matrix2)];
+
+                for (int row = 0; row < GetNumberOfRows(matrix1); row++)
+                {
+                    for (int column = 0; column < GetNumberOfColumns(matrix2); column++)
+                    {
+                        result[row, column] = 0;
+                        for (int n = 0; n < GetNumberOfRows(matrix2); n++)
+                        {
+                            result[row, column] += (matrix1[row, n] * matrix2[n, column]);
+                        }
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new Exception("invalid matrix size");
+            }
+        }
+        /// <summary>
+        /// this method multiplies matrix by scalar
+        /// </summary>
+        /// <param name="matrix">two dimentional array (double[,])</param>
+        /// <param name="number">scalar variable (double)</param>
+        /// <returns>result matrix (double[,])</returns>
+        public static double[,] Multiply(double[,] matrix, double number)
+        {
+            double[,] result = new double[GetNumberOfRows(matrix), GetNumberOfColumns(matrix)];
+            for (int row = 0; row < GetNumberOfRows(matrix); row++)
+            {
+                for (int column = 0; column < GetNumberOfColumns(matrix); column++)
+                {
+                    result[row, column] = matrix[row, column] * number;
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// this method add two matrices together
+        /// </summary>
+        /// <param name="matrix1">first two dimentional array (double[,])</param>
+        /// <param name="matrix2">second two dimentional array (double[,])</param>
+        /// <returns>result matrix (double[,])</returns>
+        /// <exception cref="Exception">
+        /// if <c>CheckForPossibleAdditionOrSubstraction</c> for these 2 matrices is false,
+        /// it throws "invalid matrix size" - it means you should check your matrices numbers of columns and rows 
+        /// </exception>
+        public static double[,] Add(double[,] matrix1, double[,] matrix2)
+        {
+            double[,] result;
+            if (CheckForPossibleAdditionOrSubstraction(matrix1, matrix2))
+            {
+                result = new double[GetNumberOfRows(matrix1), GetNumberOfColumns(matrix1)];
+                for (int row = 0; row < GetNumberOfRows(matrix1); row++)
+                {
+                    for (int column = 0; column < GetNumberOfColumns(matrix1); column++)
+                    {
+                        result[row, column] = matrix1[row, column] + matrix2[row, column];
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                throw new Exception("invalid matrix size");
+            }
+        }
+        /// <summary>
+        /// this method substract matrix2 from matrix1
+        /// </summary>
+        /// <param name="matrix1">first two dimentional array (double[,])</param>
+        /// <param name="matrix2">second two dimentional array (double[,])</param>
+        /// <returns>result matrix (double[,])</returns>
+        /// <exception cref="Exception">
+        /// if <c>CheckForPossibleAdditionOrSubstraction</c> for these 2 matrices is false,
+        /// it throws "invalid matrix size" - it means you should check your matrices numbers of columns and rows 
+        /// </exception>
+        public static double[,] Substract(double[,] matrix1, double[,] matrix2)
+        {
+            double[,] result;
+            if (CheckForPossibleAdditionOrSubstraction(matrix1, matrix2))
+            {
+                result = new double[GetNumberOfRows(matrix1), GetNumberOfColumns(matrix1)];
+                for (int row = 0; row < GetNumberOfRows(matrix1); row++)
+                {
+                    for (int column = 0; column < GetNumberOfColumns(matrix1); column++)
+                    {
+                        result[row, column] = matrix1[row, column] - matrix2[row, column];
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                throw new Exception("invalid matrix size");
+            }
+        }
+
+    }
+}
